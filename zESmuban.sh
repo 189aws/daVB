a111237a6aabb50d10b90120a37147ad73188793
189aws <189aws@gmail.com>
2025-04-26 11:42:55 +0800

    Create zESmuban.sh

---
+++ zESmuban.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 设置钱包地址
+WORKER_WALLET_ADDRESS=8sTHteGbN5bi3jRCg3XxP5LYmuBBUiDHPfLC2GUfxZES
+# 设置命令
+COMMAND_BASE="./ore-mine-pool-linux-avx512 worker --server-url http://mine.oreminepool.top/ --worker-wallet-address ${WORKER_WALLET_ADDRESS}"
+
+start_process() {    
+    local command="nohup $COMMAND_BASE >> worker.log 2>&1 &"    
+    echo "$command"
+    eval "$command"
+}
+
+ulimit -n 100000
+
+start_process
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+sleep 5
+
+while true; do
+    if ! pgrep -f "ore-mine-pool-linux-avx512" > /dev/null; then
+        echo "Process is not running, starting it..."
+        start_process
+    else
+        echo "Process is running"
+    fi
+    sleep 10
+done
