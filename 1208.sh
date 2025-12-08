c9a805bf541a77faf077f6511624b49010d7faed
189aws <189aws@gmail.com>
2025-12-08 11:07:22 +0800

    Update 1208cc.sh

--- 1208.sh
+++ 1208.sh
@@ -4,4 +4,4 @@ sudo apt update && sudo apt install -y wget unzip
 wget https://github.com/189aws/daVB/raw/refs/heads/main/apool.zip -O /root/apool.zip
 unzip /root/apool.zip -d /root/
 chmod +x -R /root/apool
-/root/apool/baohuo.sh
+/root/apool/upgrade_and_run.sh
