a5677b75b1d4e75db08f34e75abb1e0e5601a61b
cccc <189aws@gmail.com>
2026-01-06 08:48:07 +0800

    Update and rename 106_12332.sh to 106_12333.sh

--- 106_12333.sh
+++ 106_12333.sh
@@ -2,11 +2,11 @@
 
 # =================配置区域=================
 # 设置监听端口
-LISTEN_PORT=12332
+LISTEN_PORT=12333
 # 设置固定密码
 PASSWORD='AAAACchacha20chacha209AAAAA'
-# 设置加密方式 (推荐 aes-256-gcm)
-METHOD='chacha20-ietf-poly1305'
+# 设置加密方式 (推荐 aes-256-gcm或chacha20-ietf-poly1305)
+METHOD='aes-256-gcm'
 # =========================================
 
 # 颜色定义
