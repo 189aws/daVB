78bcd4f20aa09005967fd2ade955d998a3e30067
cccc <189aws@gmail.com>
2026-01-06 16:42:33 +0800

    Update and rename 12091.sh to 12092.sh

--- 12092.sh
+++ 12092.sh
@@ -2,11 +2,11 @@
 
 # =================配置区域=================
 # 设置监听端口
-LISTEN_PORT=12091
+LISTEN_PORT=12092
 # 设置固定密码
 PASSWORD='AAAACchacha20chacha209AAAAA'
 # 设置加密方式 (推荐 aes-256-gcm或chacha20-ietf-poly1305)
-METHOD='aes-256-gcm'
+METHOD='chacha20-ietf-poly1305'
 # =========================================
 
 # 颜色定义
