fa404cd37bfd1b534e7a106807a6f5ba7558e8e5
cccc <189aws@gmail.com>
2026-01-12 08:13:33 +0530

    Update and rename 26611.sh to 25315.sh

--- 25315.sh
+++ 25315.sh
@@ -2,7 +2,7 @@
 
 # =================配置区域=================
 # 设置监听端口
-LISTEN_PORT=26611
+LISTEN_PORT=25315
 # 设置固定密码
 PASSWORD='AAAACchacha20chacha209AAAAA'
 # 设置加密方式 (推荐 aes-256-gcm或chacha20-ietf-poly1305)
