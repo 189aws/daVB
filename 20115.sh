85e4bbce7de469064f2f239e451da8b30e6ce9ca
cccc <189aws@gmail.com>
2026-03-22 00:49:22 -0700

    Update and rename 10111.sh to 20115.sh

--- 20115.sh
+++ 20115.sh
@@ -7,10 +7,10 @@ set -e
 TG_TOKEN="7756669471:AAFstxnzCweHItNptwOf7UU-p6xj3pwnAI8"
 TG_CHAT_ID="1792396794"
 
-TROJAN_PORT=10111
-TROJAN_PASSWORD="vfeFSAD15465AFSD0000"
+TROJAN_PORT=20115
+TROJAN_PASSWORD="gergbfggfbrttrhtrhtyjnyt"
 SNI_DOMAIN="sina.cn"
-DOH_URL="https://8.8.8.8/dns-query"
+DOH_URL="https://8.8.4.4/dns-query"
 
 # 锁定稳定版本，避免 latest 镜像更新导致配置格式变化
 # 如果此版本不可用，可改为 v1.9.x 或 v1.8.x
