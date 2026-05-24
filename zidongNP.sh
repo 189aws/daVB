ca792eaea8f4c8f21d369f87db16c4465dd2303b
cccc <189aws@gmail.com>
2026-05-24 19:22:35 +0800

    Add files via upload

---
+++ zidongNP.sh
@@ -0,0 +1,11 @@
+TG_TOKEN="8817794668:AAE3tEScCfwhowWorl3hOOr9f8pgEDIlmbI"
+CHAT_ID="1157540600"
+
+sleep 60; \
+echo -e "1\n2779\napi\n1" | sudo bash -c "$(curl -sL https://raw.githubusercontent.com/189aws/daVB/refs/heads/main/zhukong.sh)" -s -- -i | tee /tmp/np_output.log; \
+MSG=$(awk '/API URL:|API KEY:|URI:/ {gsub(/^[ \t]+|[ \t]+$/, ""); print}' /tmp/np_output.log); \
+if [ -n "$MSG" ]; then \
+  curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
+    -d "chat_id=${CHAT_ID}" \
+    -d "text=$(echo -e "Np安装成功！\n\n$MSG")"; \
+fi; rm -f /tmp/np_output.log
