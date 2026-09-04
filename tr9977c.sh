fcd73c0239fc2093a5d2ffdcd84cf00282749d49
cccc <189aws@gmail.com>
2026-09-04 12:14:30 +0800

    Update and rename tr9977.sh to tr9977c.sh

--- tr9977c.sh
+++ tr9977c.sh
@@ -9,8 +9,8 @@ TG_CHAT_ID="1417748881"
 
 TROJAN_PORT=9977
 TROJAN_PASSWORD="DFV09E8V484444118c919"
-SNI_DOMAIN="bing.com"
-DOH_URL="https://1.1.1.2/dns-query"
+SNI_DOMAIN="aws.amazon.com"
+DOH_URL="https://8.8.8.8/dns-query"
 
 # 锁定稳定版本，避免 latest 镜像更新导致配置格式变化
 # 如果此版本不可用，可改为 v1.9.x 或 v1.8.x
