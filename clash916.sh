e51b483138d164e46f69abb6eadb50f80e787b04
cccc <189aws@gmail.com>
2026-09-12 12:16:05 +0800

    Update and rename clash915.sh to clash916.sh

--- clash916.sh
+++ clash916.sh
@@ -4,7 +4,7 @@ set -e
 # ========== 配置区 ==========
 TG_TOKEN="8896559295:AAHWVHQVJfoWG9v4McFg2qJgACw0nEpMxJo"
 TG_CHAT_ID="1417748881"
-SNI_DOMAIN="youku.com"
+SNI_DOMAIN="v.qq.com"
 
 # 自动随机生成参数
 SS_PORT=$(shuf -i 30000-45000 -n 1)        # 内部 SS 随机监听端口
